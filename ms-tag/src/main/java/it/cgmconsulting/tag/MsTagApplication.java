141cec9e3dbcfc8beff753d1d893861bb7c040e3
Cavaliere <s.lorenzo@studenti.unina.it>
2025-01-09 09:19:04 +0100

    lezione del 9-1

---
+++ ms-tag/src/main/java/it/cgmconsulting/tag/MsTagApplication.java
@@ -0,0 +1,13 @@
+package it.cgmconsulting.tag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTagApplication.class, args);
+	}
+
+}
